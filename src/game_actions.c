1478791de0f43ba4f03dcf722476dff514fe2bba
Jorge <jorge.martincastanno@estudiante.uam.es>
2025-05-05 20:24:36 +0200

    l

--- src/game_actions.c
+++ src/game_actions.c
@@ -498,7 +498,7 @@ Status game_actions_attack(Game *game)
   turn = rand() % RANDOM;
   if ((turn < 0) || turn > (RANDOM - 1))
     return ERROR;
-  if (turn > -5)
+  if (turn > (RANDOM/2))
   {
     /* If the attacking player loses a new random variable determines who loses a hitpoint (it goes from 0 up to the number of following characters)*/
     following = game_get_nfollowingcharacters(game, player_get_id(game_get_player(game)));
