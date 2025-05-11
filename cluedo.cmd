837e730975fe64275e36eef565210c6fc7c6c391
jimenasanchiz <jimena.sanchiz@estudiante.uam.es>
2025-05-11 19:23:07 +0200

    Create cluedo.cmd

---
+++ cluedo.cmd
@@ -0,0 +1,72 @@
+move east
+inspect Gasolina
+take Bidon
+take Gasolina
+take Hacha
+take Tenazas
+take Clavos
+drop gasolina
+drop Bidon
+take martillo
+move east
+move up
+move east
+move north
+move east
+use hacha
+attack Guard8
+take Key8
+open Corridor8-Comedor with Key8
+drop Key8
+drop Hacha
+move north
+move west
+chat Coronel Rubio
+recruit Coronel Rubio
+use martillo
+attack Guard2
+take Key2
+open Corridor3-Comedor with Key2
+drop Key2
+drop martillo
+move east
+take Carta Comedor
+move south
+move east
+move east
+use Tenazas over Coronel Rubio
+attack Guard4
+take Key4
+open Corridor15-Baile with Key4
+drop Key4
+drop Tenazas
+move south
+move south
+move west
+move south
+Use Clavos
+attack Guard6
+attack Guard6
+take Key6
+open Corridor14-Invernadero with Key6
+drop Key6
+move east
+chat Jardinero
+take Cuerda
+move west
+move north
+move east
+attack Guard5
+attack Guard5
+take Key5
+open Corridor16-Baile with Key5
+drop Key5
+move north
+move west
+move west
+move down
+move up
+abandon Coronel Rubio
+drop Carta Comedor
+drop Cuerda
+guess
