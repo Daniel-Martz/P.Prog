5a3f9d69f024f540000f4725236f17a70d551218
jimenasanchiz <jimena.sanchiz@estudiante.uam.es>
2025-05-11 19:22:12 +0200

    Create prueba.cmd

---
+++ prueba.cmd
@@ -0,0 +1,45 @@
+cooperate
+cooperate with Det2 
+chat
+recruit Perro
+move east
+take gasolina
+inspect gasolina
+take bidon
+take gasolina
+take martillo
+take clavos
+use gasolina
+drop gasolina
+drop bidon
+move east
+move up
+move east
+move north
+move east
+move east
+move east
+chat Sra. Celeste
+recruit Sra. Celeste
+use Clavos
+use Martillo over Sra. Celeste
+attack
+attack Guard4
+take Key4
+move down
+open Corridor15-Baile
+open Corridor15-Baile with Key4
+move south
+take Carta Sala de Baile
+take Candelabro
+move west
+move west
+move down
+move up
+abandon
+abandon Sra. Celeste
+drop
+drop Carta Sala de Baile
+drop Candelabro
+guess
+exit
